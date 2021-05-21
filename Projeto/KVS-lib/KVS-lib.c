7ee2b84b70fb43edaa246174e109a72029dd5aa7
Vitor Caires <vscaires@hotmail.com>
2021-05-21 03:58:13 -0700

    some improvements

--- Projeto/KVS-lib/KVS-lib.c
+++ Projeto/KVS-lib/KVS-lib.c
@@ -1,71 +1,62 @@
 #include "KVS-lib.h"
 
-#define SOCKET_ADDR "/tmp/KVS-LocalServer"
+#define SOCKNAME "/tmp/KVS-LocalServer"
 int sock;
 struct sockaddr_un app_sock_addr;
 
 
-int main (){    
-    char * group_id;
-    char * secret;
-    group_id = "group_id";
-    secret = "secret";
-
-    sock = socket(AF_UNIX, SOCK_STREAM, 0);
+int establish_connection (char * group_id, char * secret){    
+    int sock = socket(AF_UNIX, SOCK_STREAM, 0);
     if(sock == -1){
         perror("socket creation");
         exit(-1);
     }
 
-    memset(&app_sock_addr, 0, sizeof(struct sockaddr_un));
-    /* Clear structure */
-    app_sock_addr.sun_family = AF_UNIX;
-    /* UNIX domain address */
-    strcpy(app_sock_addr.sun_path, SOCKET_ADDR);
+    struct sockaddr_un server_addr;
 
-    if(connect(sock, & app_sock_addr, sizeof(app_sock_addr))){
+    memset(&server_addr, 0, sizeof(struct sockaddr_un));
+    /* Clear structure */
+    server_addr.sun_family = AF_UNIX;
+    /* UNIX domain address */
+    strcpy(server_addr.sun_path, SOCKNAME);
+
+    if(connect(sock, (const struct sockaddr *)&server_addr, sizeof(server_addr))){
         perror("connect");
         exit(-1);
     }
-    
-    printf("Connected.\n");
+    printf("connected\n");
 
-    char buf[1024];
-    int t;
-    while(printf("> "), fgets(buf, 1024, stdin), !feof(stdin)) {
-        if (send(sock, group_id, strlen(group_id), 0) == -1) {
-            perror("send");
-            exit(1); 
-        }
-        if (send(sock, secret, strlen(secret), 0) == -1) {
-            perror("send");
-            exit(1); 
-        }
-        if ((t=recv(sock, buf, 1024, 0)) > 0) {
-            buf[t] = '\0';
-            if(strcmp(buf, group_id) == 0){
-                printf("Group ID found\n");
-            }
-            if(strcmp(buf, secret) ==0)
-                printf("secret found\n");
-            printf("echo> %s", buf);
-        } else {
-            if (t < 0) perror("recv");
-            else printf("Server closed connection\n");
-            exit(1);
-        } 
-    }
+    char flag[8];
+    strcpy(flag, "put");
+    do{
+        sleep(1);
+    }while(write(sock, flag, sizeof(flag)) > 0);
+
+    // strcpy(group_id, "");
+    // strcpy(secret, "");
+
+    // printf("%s %ld\n", buf, sizeof(buf));
+    // do{
+    //     sleep(1);
+    // }while(write(sock, buf, sizeof(buf)) > 0);
 
     return 0;
 }
 
 
 int put_value(char * key, char * value){
-    if (send(sock, key, strlen(key), 0) == -1) {
+    char flag[8], buff[100];
+    strcpy(flag, "get");
+    if (write(sock, flag, strlen(flag)) == -1) {
+        perror("send flag");
+        exit(-1); 
+    }
+
+    if (write(sock, key, strlen(key)) == -1) {
         perror("send key");
         exit(-1); 
     }
-    if (send(sock, value, strlen(value), 0) == -1) {
+    if (write(sock, value, strlen(value)) == -1) {
         perror("send value");
         exit(-1); 
     }
@@ -74,22 +65,34 @@ int put_value(char * key, char * value){
 }
 
 int get_value(char * key, char ** value){
-    if (send(sock, key, strlen(key), 0) == -1) {
-        perror("send key");
+    char flag[8];
+    strcpy(flag, "get");
+    if (write(sock, flag, strlen(flag)) == -1) {
+        perror("send flag");
         exit(-1); 
     }
-    
-    char buf[1024];
-    if (read(sock, buf, 1024) < 0){
-        perror("receiving datagram packet");
-        return(-2);
+
+    if (write(sock, key, strlen(key)) == -1) {
+        perror("send key");
+        exit(-2); 
     }
+    // malloc
+    do{
+        sleep(1);
+    }while(read(sock, value, sizeof(value)) > 0);
 }
 
 int delete_value(char * key){
+    char flag[8];
+    strcpy(flag, "delete");
+    if (write(sock, flag, strlen(flag)) == -1) {
+        perror("send flag");
+        exit(-1); 
+    }
+
     if (send(sock, key, strlen(key), 0) == -1) {
         perror("send key");
-        exit(-1); 
+        exit(-2); 
     }
 }
 
