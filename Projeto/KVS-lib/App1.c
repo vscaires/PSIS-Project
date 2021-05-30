7091e1e9c63f05df08665ba98b7df8602df3aede
Vitor Caires <vscaires@hotmail.com>
2021-05-29 19:02:52 -0700

    dictionary not working properly

--- Projeto/KVS-lib/App1.c
+++ Projeto/KVS-lib/App1.c
@@ -8,13 +8,13 @@ int main(){
     char ** msg;
 
     strcpy(key, "abcd");
-    strcpy(value, "10238407123469127346\0"); 
+    strcpy(value, "10238407123469127346"); 
     int sock; 
 
     establish_connection(group_id, secret);
     put_value(key, value);
     put_value(key, value);
-    put_value(key, value);
+
     strcpy(key, "abc"); 
     put_value(key, value);
     strcpy(key, "ab");
