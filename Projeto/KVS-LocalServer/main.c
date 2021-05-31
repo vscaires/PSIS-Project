eac36e8e88afebecbf1e36b628821ccfcd31cd5b
Vitor Caires <vscaires@hotmail.com>
2021-05-31 02:58:33 -0700

    get_value implemented

--- Projeto/KVS-LocalServer/main.c
+++ Projeto/KVS-LocalServer/main.c
@@ -5,7 +5,7 @@ int main(){
     int client_socket, i = 0;
     char key[256], c[1], *value;
     struct sockaddr_un server_addr;
-    dict_t **kvs = dictAlloc();
+    Dict kvs = DictCreate();
 
     memset(&server_addr, 0, sizeof(struct sockaddr_un));
     /* Clear structure */
@@ -42,8 +42,8 @@ int main(){
                 value = malloc(i*sizeof(char));
                 read(client_socket, value, i);
                 
-                if(getItem(*kvs, key) == NULL || getItem(*kvs, key) != value){
-                    addItem(kvs, key, value);
+                if(DictSearch(kvs, key) == 0 || strcmp(DictSearch(kvs, key), value) != 0){
+                    DictInsert(kvs, key, value);
                     printf("Key-value added\n");
                 }else{
                     printf("Key-value already exists\n");   
@@ -55,15 +55,15 @@ int main(){
                 read(client_socket, &i, sizeof(i));
                 read(client_socket, key, i);
 
-                if(getItem(*kvs, key) == NULL){
+                if(DictSearch(kvs, key) == 0){
                     i = -1;
                     write(client_socket, &i, sizeof(i));
                     printf("Key-value not found ...\n");
                 }else{
-                    (char*)getItem(*kvs, key); //incomplete
-                    i = strlen((char*)getItem(*kvs, key));
+                    printf("Value : %s\n", DictSearch(kvs, key));
+                    i = strlen(DictSearch(kvs, key));
                     write(client_socket, &i, sizeof(i));
-                    write(client_socket, (char*)getItem(*kvs, key), i);
+                    write(client_socket, DictSearch(kvs, key), i);
                     printf("Key-value found!\n");   
                 }
                 memset(key, 0, sizeof(key));
@@ -73,71 +73,197 @@ int main(){
                 read(client_socket, &i, sizeof(i));
                 read(client_socket, key, i);
 
-                if(getItem(*kvs, key) == NULL){
+                if(DictSearch(kvs, key) == NULL){
                     printf("Key-value not found ...\n");
                 }else{
-                    delItem(kvs, key);
+                    DictDelete(kvs, key);
                     printf("Key-value found and deleted!\n");   
                 }
             }
         }while(read(client_socket, &i, sizeof(i)) > 0);
-
+        
         close(client_socket);
         printf("Socket closed\n");
     }
-    dictDealloc(kvs);
+    DictDestroy(kvs);
+    printf("Key-Value data destroyed! \n");
+
     return 0;
 }
 
-dict_t **dictAlloc(void) {
-    return malloc(sizeof(dict_t));
+struct elt {
+    struct elt *next;
+    char *key;
+    char *value;
+};
+
+struct dict {
+    int size;           /* size of the pointer table */
+    int n;              /* number of elements stored */
+    struct elt **table;
+};
+
+#define INITIAL_SIZE (1024)
+#define GROWTH_FACTOR (2)
+#define MAX_LOAD_FACTOR (1)
+
+/* dictionary initialization code used in both DictCreate and grow */
+Dict internalDictCreate(int size)
+{
+    Dict d;
+    int i;
+
+    d = malloc(sizeof(*d));
+
+    assert(d != 0);
+
+    d->size = size;
+    d->n = 0;
+    d->table = malloc(sizeof(struct elt *) * d->size);
+
+    assert(d->table != 0);
+
+    for(i = 0; i < d->size; i++) d->table[i] = 0;
+
+    return d;
 }
 
-void dictDealloc(dict_t **dict) {
-    free(dict);
+Dict DictCreate(void)
+{
+    return internalDictCreate(INITIAL_SIZE);
 }
 
-void *getItem(dict_t *dict, char *key) {
-    dict_t *ptr;
-    for (ptr = dict; ptr != NULL; ptr = ptr->next) {
-        if (strcmp(ptr->key, key) == 0) {
-            return ptr->value;
+void DictDestroy(Dict d)
+{
+    int i;
+    struct elt *e;
+    struct elt *next;
+
+    for(i = 0; i < d->size; i++) {
+        for(e = d->table[i]; e != 0; e = next) {
+            next = e->next;
+
+            free(e->key);
+            free(e->value);
+            free(e);
         }
     }
-    
-    return NULL;
+
+    free(d->table);
+    free(d);
 }
 
-void delItem(dict_t **dict, char *key) {
-    dict_t *ptr, *prev;
-    for (ptr = *dict, prev = NULL; ptr != NULL; prev = ptr, ptr = ptr->next) {
-        if (strcmp(ptr->key, key) == 0) {
-            if (ptr->next != NULL) {
-                if (prev == NULL) {
-                    *dict = ptr->next;
-                } else {
-                    prev->next = ptr->next;
-                }
-            } else if (prev != NULL) {
-                prev->next = NULL;
-            } else {
-                *dict = NULL;
-            }
-            
-            free(ptr->key);
-            free(ptr);
-            
+#define MULTIPLIER (97)
+
+static unsigned long hash_function(const char *s)
+{
+    unsigned const char *us;
+    unsigned long h;
+
+    h = 0;
+
+    for(us = (unsigned const char *) s; *us; us++) {
+        h = h * MULTIPLIER + *us;
+    }
+
+    return h;
+}
+
+static void grow(Dict d)
+{
+    Dict d2;            /* new dictionary we'll create */
+    struct dict swap;   /* temporary structure for brain transplant */
+    int i;
+    struct elt *e;
+
+    d2 = internalDictCreate(d->size * GROWTH_FACTOR);
+
+    for(i = 0; i < d->size; i++) {
+        for(e = d->table[i]; e != 0; e = e->next) {
+            /* note: this recopies everything */
+            /* a more efficient implementation would
+             * patch out the strdups inside DictInsert
+             * to avoid this problem */
+            DictInsert(d2, e->key, e->value);
+        }
+    }
+
+    /* the hideous part */
+    /* We'll swap the guts of d and d2 */
+    /* then call DictDestroy on d2 */
+    swap = *d;
+    *d = *d2;
+    *d2 = swap;
+
+    DictDestroy(d2);
+}
+
+/* insert a new key-value pair into an existing dictionary */
+void DictInsert(Dict d, const char *key, const char *value)
+{
+    struct elt *e;
+    unsigned long h;
+
+    assert(key);
+    assert(value);
+
+    e = malloc(sizeof(*e));
+
+    assert(e);
+
+    e->key = strdup(key);
+    e->value = strdup(value);
+
+    h = hash_function(key) % d->size;
+
+    e->next = d->table[h];
+    d->table[h] = e;
+
+    d->n++;
+
+    /* grow table if there is not enough room */
+    if(d->n >= d->size * MAX_LOAD_FACTOR) {
+        grow(d);
+    }
+}
+
+/* return the most recently inserted value associated with a key */
+/* or 0 if no matching key is present */
+const char *DictSearch(Dict d, const char *key)
+{
+    struct elt *e;
+
+    for(e = d->table[hash_function(key) % d->size]; e != 0; e = e->next) {
+        if(!strcmp(e->key, key)) {
+            /* got it */
+            return e->value;
+        }
+    }
+
+    return 0;
+}
+
+/* delete the most recently inserted record with the given key */
+/* if there is no such record, has no effect */
+void DictDelete(Dict d, const char *key)
+{
+    struct elt **prev;          /* what to change when elt is deleted */
+    struct elt *e;              /* what to delete */
+
+    for(prev = &(d->table[hash_function(key) % d->size]); 
+        *prev != 0; 
+        prev = &((*prev)->next)) {
+        if(!strcmp((*prev)->key, key)) {
+            /* got it */
+            e = *prev;
+            *prev = e->next;
+
+            free(e->key);
+            free(e->value);
+            free(e);
+
             return;
         }
     }
 }
 
-void addItem(dict_t **dict, char *key, void *value) {
-    delItem(dict, key); /* If we already have a item with this key, delete it. */
-    dict_t *d = malloc(sizeof(struct dict_t_struct));
-    d->key = malloc(strlen(key)+1);
-    strcpy(d->key, key);
-    d->value = value;
-    d->next = *dict;
-    *dict = d;
-}
